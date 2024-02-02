684d9787ce32ba786d67e26a41b31d3d755b942a
Sidd03192 <135279087+Sidd03192@users.noreply.github.com>
2024-02-02 13:36:13 -0600

    update QuestionsNode

--- QuestionsGame.java
+++ QuestionsGame.java
@@ -6,7 +6,31 @@
 public class QuestionsGame {
     // Your code here
 
+    public QuestionsGame(String object) {
+        // initilize a new QuetionsGame object with a single leaf node representing the object object. 
+
+
+    }
+
+
+
     private static class QuestionNode {
         // Your code here
+        public String question; // data stored at main node
+        public QuestionNode left; // left node
+        public QuestionNode right; // right node
+
+        public QuestionNode (String question) 
+        {
+            this(question, null, null);
+        }
+
+
+    // Constructs a branch node with the given data and links
+
+        public QuestionNode(String question, QuestionNode left, QuestionNode right) {
+            this.question = question;
+            this.left = left;
+            this.right = right;
     }
 }
