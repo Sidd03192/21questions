c68d654a5d3066094e1df5e8e926700162ad29cd
Sidd03192 <135279087+Sidd03192@users.noreply.github.com>
2024-02-05 14:39:28 -0600

    monkey method update 1

--- QuestionsGame.java
+++ QuestionsGame.java
@@ -2,19 +2,50 @@
 //
 // You should delete this comment and replace it with your class
 // header comment.
+import java.io.File;
+import java.io.FileNotFoundException;
 import java.util.*;
-public class QuestionsGame {
+public class QuestionsGame { 
     // Your code here
-
+        QuestionNode Panda;
     public QuestionsGame(String object) {
         // initilize a new QuetionsGame object with a single leaf node representing the object object. 
-        QuestionNode monkey = new QuestionNode(object);
+         Panda = new QuestionNode(object);
 
     }
-    public QuestionsGame(Scanner input){
-        while(input.hasNext()){
+    public QuestionsGame(Scanner input) throws FileNotFoundException{
+        input = new Scanner(new File("spec-questions.txt"));
+        input.nextLine();
+        Panda = new QuestionNode(input.next());
+        monkey( input, Panda);
 
+    }
+
+    private   void monkey (Scanner input, QuestionNode node) throws FileNotFoundException{
+        
+
+        
+        if ( input.nextLine().equals("Q:")){
+
+         node.left = new QuestionNode(input.nextLine());
+         System.out.println(node.left.data);
+         monkey (input, node.left);
         }
+
+        if( input.nextLine().equals("A:"))
+        {
+            node.right = new QuestionNode(input.nextLine());
+            System.out.println(node.left.data);
+            monkey(input, node.right);
+        }
+
+        
+    
+
+        monkey(input, node);
+
+
+
     }
 
     
